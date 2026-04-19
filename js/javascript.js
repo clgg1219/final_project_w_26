3f167e751cde3be5ce5a43ecadd58eafc4b6465c
clgg1219 <cagong@umich.edu>
2026-04-19 18:33:49 -0400

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -28,3 +28,53 @@ mute.addEventListener("click", function () {
     }
 })
 
+let hide = document.querySelector(".hide");
+hide.addEventListener("click", function () {
+    document.querySelector(".content").style.display = "none";
+    document.querySelector(".show").style.display = "block";
+    console.log("hid content");
+})
+
+let show = document.querySelector(".show");
+show.addEventListener("click", function () {
+    document.querySelector(".content").style.display = "block";
+    document.querySelector(".show").style.display = "none";
+    console.log("shown content");
+})
+
+let stop_storm = document.getElementById("stop_storm");
+stop_storm.addEventListener("click", function() {
+    if (stop_storm.textContent == "stop animation") {
+        document.body.style.backgroundImage = "url(/media/storm_still.png)";
+        stop_storm.textContent = "start animation";
+        console.log("stopped animation");
+    } else {
+        document.body.style.backgroundImage = "url(/media/storm.gif)";
+        stop_storm.textContent = "stop animation";
+        console.log("started animation");
+    }
+})
+
+let stop_beach = document.getElementById("stop_beach");
+stop_beach.addEventListener("click", function() {
+    if (stop_beach.textContent == "stop animation") {
+        document.body.style.backgroundImage = "url(/media/beach_still.png)";
+        stop_beach.textContent = "start animation";
+        console.log("stopped animation");
+    } else {
+        document.body.style.backgroundImage = "url(/media/beach.gif)";
+        stop_beach.textContent = "stop animation";
+        console.log("started animation");
+    }
+})
+
+let stop_rain = document.getElementById("stop_rain");
+stop_rain.addEventListener("click", function() {
+    if (stop_rain.textContent == "stop animation") {
+        document.body.style.backgroundImage = "url(/media/rain_still.png)";
+        stop_rain.textContent = "start animation";
+    } else {
+        document.body.style.backgroundImage = "url(/media/rain.gif)";
+        stop_rain.textContent = "stop animation";
+    }
+})
